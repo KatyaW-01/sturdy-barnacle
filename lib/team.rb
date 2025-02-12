458e23250f6fc540e777ce7d46e307406c332284
Katya Weicht <katya.weicht@gmail.com>
2025-02-12 10:47:57 -0700

    Add methods

--- lib/team.rb
+++ lib/team.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Team
     attr_reader :name, :city, :roster
     def initialize (name, city)
@@ -15,4 +16,28 @@ class Team
     def add_player(player)
         @roster << player
     end
+
+    def long_term_players
+        @roster.find_all do |roster|
+            roster.contract_length > 24
+        end
+    end
+
+    def short_term_players
+        @roster.find_all do |roster|
+            roster.contract_length <= 24
+        end
+    end
+
+    def total_value_array
+        @roster.map do |roster|
+            roster.total_cost
+        end
+    end
+
+    def total_value
+        total_value_array.sum
+    end
+
+    
 end
